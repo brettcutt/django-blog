6e952977b1b1dd537c2325ac182a5f9517786c41
Brett Cutting <brettcutt@outlook.com.au>
2018-11-14 02:41:20 +0000

    deleted env

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ.setdefault("SECRET_KEY", "'s*9hx4b8a7-j4f#0%yrktai=7s3f5ihbht@f8hi_z3i52gnc5i'")
-
-os.environ.setdefault("DATABASE_URL", "postgres://zutvckfyemdlii:1c620fea76cb0c1684817c64a450f12fd134c7e4e452f859accccc255fd21b49@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/d7o3gpbpp7d90f")
