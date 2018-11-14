c5b65d824bb66c7ef100ea6d4dcb385cf567375e
Brett Cutting <brettcutt@outlook.com.au>
2018-11-14 01:00:37 +0000

    added heroku and progres support for deployment

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+os.environ.setdefault("SECRET_KEY", "'@^#yo215c8_v9q889cnobk4on00@7zfp(=loo@yf9s^7#4t7yy'")
+
+os.environ.setdefault("DATABASE_URL", "postgres://admjljwpemhrua:2f336e248fcebd073c64c295d97cc72b4e5525b67d9fa5d49590de6c3d481e5c@ec2-54-217-250-0.eu-west-1.compute.amazonaws.com:5432/d3398sp5tljhkp")
